b6a7c47286b0a8cca5941cb3b1eddafed7d493f9
Leo jPod <leo.jpod@lilo.org>
2020-02-11 17:42:30 +0100

    almost there ...

--- src/elm/Elements.elm
+++ src/elm/Elements.elm
@@ -1,28 +1,131 @@
-module Elements exposing (box, dropdown)
+module Elements exposing
+    ( Option
+    , Selection
+    , box
+    , dropdown
+    , updateSelectionOnId
+    )
 
 import FontAwesome as Icon
-import Html exposing (Html, button, div, h1, span, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, h1, input, label, span, text)
+import Html.Attributes exposing (class, classList, type_)
+import Html.Events exposing (onClick, onInput)
 
 
-dropdown : () -> Html msg
-dropdown _ =
-    div [ class "dropdown" ]
-        [ div [ class "dropdown-trigger" ]
-            [ button [ class "button is-rounded is-small has-background-grey-lighter" ]
-                [ span [ class "uppercase mr-2" ] [ text "compare" ]
-                , span [ class "icon is-small" ] [ Icon.icon Icon.caretDown ]
-                ]
+
+-- Model like stuff
+
+
+type alias Option a =
+    { a
+        | id : String
+        , name : String
+        , selected : Bool
+    }
+
+
+type alias Selection a =
+    List (Option a)
+
+
+selectedOptions : Selection a -> List String
+selectedOptions =
+    List.filter .selected
+        >> List.map .name
+
+
+updateSelectionOnId : Option a -> Selection a -> Selection a
+updateSelectionOnId replacement =
+    List.map
+        (\({ id } as option) ->
+            if id == replacement.id then
+                replacement
+
+            else
+                option
+        )
+
+
+
+-- box element
+
+
+type alias Messages a msg =
+    { toggleOption : Option a -> msg
+    , showDropdown : msg
+    , hideDropdown : msg
+    }
+
+
+box : Messages a msg -> String -> List (Html.Attribute msg) -> Selection a -> Bool -> Html msg
+box messages title extraAttributes selection isShown =
+    div (class "box h-64" :: extraAttributes)
+        [ div [ class "flex flex-row-reverse items-center justify-between" ]
+            [ dropdown messages selection isShown
+            , h1 [ class "title uppercase is-size-4" ] [ text title ]
             ]
+        , case selectedOptions selection of
+            [] ->
+                text ""
+
+            selected ->
+                String.join ", " selected
+                    |> (++) "Selected: "
+                    |> text
         ]
 
 
-box : String -> List (Html.Attribute msg) -> List e -> List e -> Bool -> Html msg
-box title extraAttributes selected options showDropdown =
-    div (class "box h-64" :: extraAttributes)
-        [ div [ class "flex flex-row-reverse items-center justify-between" ]
-            [ dropdown ()
-            , h1 [ class "title uppercase is-size-4" ] [ text title ]
+dropdown : Messages a msg -> Selection a -> Bool -> Html msg
+dropdown { toggleOption, showDropdown, hideDropdown } selection isShown =
+    div [ class "dropdown is-right", classList [ ( "is-active", isShown ) ] ] <|
+        div [ class "dropdown-trigger" ]
+            [ button
+                [ class "button is-rounded is-small has-background-grey-lighter"
+                , onClick showDropdown
+                ]
+                [ span [ class "uppercase mr-2" ] [ text "compare" ]
+                , span [ class "icon is-small" ]
+                    [ Icon.icon <|
+                        if isShown then
+                            Icon.caretUp
+
+                        else
+                            Icon.caretDown
+                    ]
+                ]
             ]
-        , text "here will be options"
+            :: (if isShown then
+                    -- TODO put the menu here
+                    [ -- the dropback first
+                      div
+                        [ class "fixed left-0 top-0 right-0 bottom-0"
+                        , onClick hideDropdown
+                        ]
+                        []
+                    , div [ class "dropdown-menu" ]
+                        [ div [ class "dropdown-content overflow-y-auto h-48" ] <|
+                            List.map (viewOption toggleOption) selection
+                        ]
+                    ]
+
+                else
+                    []
+               )
+
+
+viewOption : (Option a -> msg) -> Option a -> Html msg
+viewOption toggleOption ({ name, selected } as option) =
+    div [ class "px-3 py-2 whitespace-no-wrap" ]
+        [ label [ class "checkbox" ]
+            [ input
+                [ type_ "checkbox"
+                , Html.Attributes.selected selected
+                , onInput <| \_ -> toggleOption option
+                , class "mr-2"
+                ]
+                []
+            , span [ class "whitespace-no-wrap" ]
+                [ text name
+                ]
+            ]
         ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -11,13 +11,42 @@ import Html.Attributes exposing (class, src)
 ---- MODEL ----
 
 
+type Section
+    = Overall
+    | Category Int
+
+
 type alias Model =
-    {}
+    { overall : Elements.Selection {}
+    , category1 : Elements.Selection {}
+    , category2 : Elements.Selection {}
+    , category3 : Elements.Selection {}
+    , activeDropdown : Maybe Section
+    }
+
+
+mockSelectionWithPrefix : String -> Int -> Int -> Elements.Selection {}
+mockSelectionWithPrefix prefix from to =
+    List.range from to
+        |> List.map
+            (\idx ->
+                { id = prefix ++ String.fromInt idx
+                , name = "Option " ++ prefix ++ String.fromInt idx
+                , selected = False
+                }
+            )
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { overall = mockSelectionWithPrefix "" 1 15
+      , category1 = mockSelectionWithPrefix "1." 1 10
+      , category2 = mockSelectionWithPrefix "2." 1 10
+      , category3 = mockSelectionWithPrefix "3." 1 10
+      , activeDropdown = Nothing
+      }
+    , Cmd.none
+    )
 
 
 
@@ -25,23 +54,62 @@ init =
 
 
 type Msg
-    = NoOp
+    = ToggleDropdown (Maybe Section)
+      -- this might not be the optimal way but ... it will do for a small example
+    | ToggleOption (Elements.Option {})
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        ToggleDropdown dropdown ->
+            ( { model | activeDropdown = dropdown }, Cmd.none )
+
+        ToggleOption ({ selected } as option) ->
+            model.activeDropdown
+                |> Maybe.map
+                    (\section ->
+                        case section of
+                            Overall ->
+                                ( { model | overall = Elements.updateSelectionOnId { option | selected = not selected } model.overall }, Cmd.none )
+
+                            Category 1 ->
+                                ( { model | category1 = Elements.updateSelectionOnId { option | selected = not selected } model.category1 }, Cmd.none )
+
+                            Category 2 ->
+                                ( { model | category2 = Elements.updateSelectionOnId { option | selected = not selected } model.category2 }, Cmd.none )
+
+                            Category 3 ->
+                                ( { model | category3 = Elements.updateSelectionOnId { option | selected = not selected } model.category3 }, Cmd.none )
+
+                            _ ->
+                                ( model, Cmd.none )
+                    )
+                |> Maybe.withDefault ( model, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+isActive : Section -> Model -> Bool
+isActive section { activeDropdown } =
+    Just section == activeDropdown
+
+
 view : Model -> Html Msg
-view model =
+view ({ overall, category1, category2, category3 } as model) =
     div [ class "min-h-screen min-w-screen has-background-white-ter" ]
         [ div [ class "container is-rounded has-background-grey-lighter" ]
-            [ Elements.box "Overall" [] [] [] False
+            [ Elements.box
+                { toggleOption = ToggleOption
+                , showDropdown = ToggleDropdown <| Just Overall
+                , hideDropdown = ToggleDropdown Nothing
+                }
+                "Overall"
+                []
+                overall
+                (isActive Overall model)
             , div [ class "container p-5" ]
                 [ div [ class "m-0" ]
                     [ h1 [ class "title uppercase is-size-4 m-0" ] [ text "Breakdown" ]
@@ -50,15 +118,39 @@ view model =
                 ]
             , div [ class "container" ]
                 [ div [ class "columns mx-5 pb-8" ]
-                    [ Elements.box "Category 1" [ class "column is-full" ] [] [] False
+                    [ Elements.box
+                        { toggleOption = ToggleOption
+                        , showDropdown = ToggleDropdown <| Just <| Category 1
+                        , hideDropdown = ToggleDropdown Nothing
+                        }
+                        "Category 1"
+                        [ class "column is-full" ]
+                        category1
+                        (isActive (Category 1) model)
                     ]
                 , div [ class "columns mx-3 pb-8" ]
                     [ div [ class "column columns" ]
                         [ div [ class "column is-half" ]
-                            [ Elements.box "Category 2" [] [] [] False
+                            [ Elements.box
+                                { toggleOption = ToggleOption
+                                , showDropdown = ToggleDropdown <| Just <| Category 2
+                                , hideDropdown = ToggleDropdown Nothing
+                                }
+                                "Category 2"
+                                []
+                                category2
+                                (isActive (Category 2) model)
                             ]
                         , div [ class "column is-half" ]
-                            [ Elements.box "Category 3" [] [] [] False
+                            [ Elements.box
+                                { toggleOption = ToggleOption
+                                , showDropdown = ToggleDropdown <| Just <| Category 3
+                                , hideDropdown = ToggleDropdown Nothing
+                                }
+                                "Category 3"
+                                []
+                                category3
+                                (isActive (Category 3) model)
                             ]
                         ]
                     ]
